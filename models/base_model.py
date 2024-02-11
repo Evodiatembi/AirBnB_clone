9971debbeb5267521b7dab0d5169621b6d8de1d9
Buchiken11 <buchiken025@gmail.com>
2024-02-11 23:55:29 +0100

    task 7

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,42 @@
 #!/usr/bin/python3
+
 import uuid
+
+# from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """this class defines all common attributes or methos for other class. i.e class where other classes will inherit from
-    """
-
     def __init__(self, *args, **kwargs):
-        """
-        instantiation construct of attribute
-        """
-        mode_time = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
-            for key, value in kwargs.item():
+            for key, value in kwargs.items():
                 if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strtime(value, mode_time))
-                else:
-                    setattr(self, key, value)
+                    continue  # skip it
+
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.fromisoformat(value)
+
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        """
-        prints the class name, self.id and self.dict in dictionary representation
-        """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}]".format(class_name, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        """
-        udates the public instance attribute updated_at with the current datetime
-        """
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
-        """
-        This returns the a dictionary containing keys and valuesof the dict of the instance
-        """
-        only_instance_to_dict = self.__dict__.copy()
-        #class_name = self.__class__.__name__
-        only_instance_to_dict["__class__"] = self.__class__.__name__
-        only_instance_to_dict["created_at"] = self.created_at.isoformat()
-        only_instance_to_dict["updated_at"] = self.updated_at.isoformat()
-        return only_instance_to_dict
+        # instance_dict = dict(self.__dict__)
+        inst_dict = self.__dict__.copy()
 
-if __name__ == "__main__":
-    my_class_model = BaseModel()
-    my_class_model.name = "Buchi and Tembi"
-    my_class_model.my_number = 89
-    print(my_class_model)
-    my_class_model.save()
-    print(my_class_model)
-    my_class_model_json = my_class_model.to_dict()
-    print(my_class_model_json)
-    print("Jason of my_class_model:")
-    for key in my_class_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_class_model_json[key]), my_class_model_json[key]))
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+        inst_dict["created_at"] = self.created_at.isoformat()
+
+        return inst_dict
