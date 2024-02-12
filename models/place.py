cb54620c808d153e49c50ab71cba05a1313c1377
Evodiatembi <evodytembi@gmail.com>
2024-02-11 23:50:57 -0800

    place.py

--- models/place.py
+++ models/place.py
@@ -4,22 +4,27 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
+
     """Represent the class place.
 
+
     Attributes:
-        city_id(string): The City id
-        user_id(string): The User id
-        name(string): The name of the place 
-        description(string): The description of the place
-        number_rooms(integer): The number of rooms of the place
-        number_bathrooms(integer): The number of bathrooms of the place
-        max_guest(integer): The maximum number of guests of the place
-        price_by_night(integer): The price by night of the place
-        latitude(float): The latitude of the place
-        longitude(float): The longitude of the place
-        amenity_ids(list): A list of Amenity ids
+
+        city_id (string): The City id
+        user_id (string): The User id
+        name (string): The name of the place 
+        description (string): The description of the place
+        number_rooms (integer): The number of rooms of the place
+        number_bathrooms (integer): The number of bathrooms of the place
+        max_guest (integer): The maximum number of guests of the place
+        price_by_night (integer): The price by night of the place
+        latitude (float): The latitude of the place
+        longitude (float): The longitude of the place
+        amenity_ids (list): A list of Amenity ids
     """
+
     def __init__(self):
+
        self.city_id = ""
        self.user_id = ""
        self.name = ""
