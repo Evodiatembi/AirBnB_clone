8f383491b916be74fff4ee821ca8bbe2c85eb3f4
Buchiken11 <buchiken025@gmail.com>
2024-02-13 20:59:49 +0100

    updated task

--- console.py
+++ console.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
 import cmd
+import shlex
 import sys
 from models.base_model import BaseModel
+from models.engine import storage
 #from models.user import user
- models import storage
-from models.user import user
 #from models import storage
->>>>>>> parent of 446c694 (updated)
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "hbnb "
     my_class_name = ["BaseModel"]
 
-    def do_emptyline(self):
+    def emptyline(self):
         """ This does nothing when an empty-line is entered in the console"""
         pass
 
     def do_create(self, line):
         ''' This creates a user with a unique identification'''
-        if len(line.split()) != 2:
+        command_line = shlex.split(line)
+        if len(command_line) == 0:
             print("** class name missing **")
-        elif line.split()[0] not in self.my_class_name:
+        elif command_line[0] not in self.my_class_name:
             print("** class doesn't exist **")
         else:
             new_created_instance = BaseModel()
@@ -31,15 +31,16 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, line):
         """ Shows whatever that has been created by the base class"""
-        if len(line.split()) == 0:
+        command_line = shlex.split(line)
+        if len(command_line) == 0:
             print("** class name missing **")
-        elif line.split()[0] not in self.my_class_name:
+        elif command_line[0] not in self.my_class_name:
             print("** class doesn't exist **")
-        elif len(line.split()) < 2:
+        elif len(command_line) < 2:
             print("** instance id missing **")
         else:
             objects = storage.all()
-            key = "{}.{}".format(line.split()[0], line.split()[1])
+            key = "{}.{}".format(command_line[0], command_line()[1])
             if key in objects:
                 print(objects[key])
             else:
@@ -50,15 +51,16 @@ class HBNBCommand(cmd.Cmd):
         This method destroys whatever instance or
         attributes that is no longer needed
         """
-        if len(line.split()) == 0:
+        command_line = shlex.split(line)
+        if len(command_line) == 0:
             print("** class name missing **")
-        elif line.split()[0] not in self.my_class_name:
+        elif command_line[0] not in self.my_class_name:
             print("** class doesn't exist **")
-        elif len(line.split()) < 2:
+        elif len(command_line) < 2:
             print("** instance id missing **")
         else:
             objects = storage.all()
-            key = "{}.{}".format(line.split()[0], line.split()[1])
+            key = "{}.{}".format(command_line[0], command_line()[1])
             if key in objects:
                 del objects[key]
             else:
@@ -68,15 +70,16 @@ class HBNBCommand(cmd.Cmd):
         """
         This method prints all instances of all classes as a string
         """
+        command_line = shlex.split(line)
         objects = storage.all()
-        if len(line.split()) == 0:
+        if len(command_line) == 0:
             for key, value in objects.items():
                 print(str(value))
-        elif line.split()[0] not in self.my_class_name:
+        elif command_line[0] not in self.my_class_name:
             print("** class doesn't exist **")
         else:
             for key, value in objects.items():
-                if key.split('.')[0] == line.split()[0]:
+                if key.split('.')[0] == command_line[0]:
                     print(str(value))
 
     def do_update(self, line):
@@ -84,25 +87,26 @@ class HBNBCommand(cmd.Cmd):
         Updates an instance of the class that was created,
         i.e <class name> <id> <attribute_name>
         """
-        if len(line.split()) == 0:
+        command_line = shlex.split(line)
+        if len(command_line) == 0:
             print("** class name missing **")
-        elif line.split()[0] not in self.my_class_name:
+        elif command_line[0] not in self.my_class_name:
             print("** class doesn't exist **")
-        elif len(line.split()) < 2:
+        elif len(command_line) < 2:
             print("** instance id missing **")
         else:
             objects = storage.all()
-            key = "{}.{}".format(line.split()[0], line.split()[1])
+            key = "{}.{}".format(command_line[0], command_line[1])
             if key not in objects:
                 print("** no instance found **")
-            elif len(line.split()) < 3:
+            elif len(command_line) < 3:
                 print("** attribute name missing **")
-            elif len(line.split()) < 4:
+            elif len(command_line) < 4:
                 print("** value missing **")
             else:
                 obj = objects[key]
-                attr_name = line.split()[2]
-                attr_value = line.split()[3]
+                attr_name = comman_line[2]
+                attr_value = comman_line[3]
                 try:
                     attr_value = eval(attr_value)
                 except Exception:
@@ -125,8 +129,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-    if sys.stdin.isatty():
-       # hbnb_cmd.cmdloop()
-    else:
-        for line in sys.stdin:
-            hbnb_cmd.onecmd(line.strip())
