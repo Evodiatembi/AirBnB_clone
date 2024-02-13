1235278b99498b0a8a51c0d88297f1fda0e181bf
Buchiken11 <buchiken025@gmail.com>
2024-02-13 21:00:27 +0100

    updated task

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
-                    continue  # skip it
+                    continue
 
                 if key in ["created_at", "updated_at"]:
                     value = datetime.fromisoformat(value)
@@ -22,14 +22,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
-            models.storage.new(self)
+            #models.__init__.storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.utcnow()
-        models.storage.save()
+        #models.storage.save()
 
     def to_dict(self):
 
